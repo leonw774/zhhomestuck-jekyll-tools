b4f988ffdc55a7be53d38c3fc10ee3e9b2bcd36a
leonw774 <leonw774@gmail.com>
2019-01-30 23:03:42 +0800

    update style setting

--- build_pages.bat
+++ build_pages.bat
@@ -1,7 +1,10 @@
 rem ruby -rubygems -e 'require "jekyll-import";JekyllImport::Importers::Blogger.run({"source"=^>"./blog.xml",})'
 python give_layouts.py
 call jekyll b
+echo remove unwanted tags
 python p_tag_remover.py
+echo copy pages
 for /r "./pages" %%f in (*.html) do copy pages\%%~nxf zhhomestuck.github.io\p
+echo make blog_raw.txt
 python make_raw_blog.py
 echo done.
