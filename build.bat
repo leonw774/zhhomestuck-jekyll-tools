ec7f4d6596f492e433f342e5cc7133446de91db0
leonw774 <leonw774@gmail.com>
2019-04-13 22:45:34 +0800

    Updated index.html

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rem ruby -rubygems -e 'require "jekyll-import";JekyllImport::Importers::Blogger.run({"source"=^>"./blog.xml",})'
-rem python give_layouts.py
-rem call jekyll b
+python give_layouts.py
+call jekyll b
 
 echo removing unwanted tags
 python p_tag_remover.py
@@ -16,5 +16,6 @@ copy blog.xml ..\zhhomestuck.github.io\blog-backup\blog.xml
 echo make index.html
 python index_updater.py
 copy index_updated.html ..\zhhomestuck.github.io\index.html
+del index_updated.html
 
 echo done.
