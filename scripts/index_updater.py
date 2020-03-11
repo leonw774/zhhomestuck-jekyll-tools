f4d4d91e96dfb49a424bc1d24d3ae40b9ec21db4
leonw774 <leonw774@gmail.com>
2020-03-11 22:20:44 +0800

    update index.html and fix script

--- scripts/index_updater.py
+++ scripts/index_updater.py
@@ -10,7 +10,7 @@ except:
 
 file_content = file.read()
 now_date = datetime.now()
-file_content = file_content.replace("date_now", now_date.strftime("%Y-%m-%d"))
+file_content = file_content.replace("now_date", now_date.strftime("%Y-%m-%d"))
 file.close
 
 pagename_list = os.listdir("_posts")
@@ -18,18 +18,18 @@ pagename_list = os.listdir("_posts")
 update_date = pagename_list[-1][:10]
 update_list = [x for x in pagename_list if x[:10]==update_date]
 update_list.sort()
+update_page_link = "/p/" + update_list[0][11:17]
 
 update_page_title = ""
 with open("_posts/"+update_list[0], "r", encoding = "utf-8-sig") as f:
     for line in f.readlines():
         if line[0:5]=="title":
-            update_page_title = line[8:-1]
+            update_page_title = line[7:-1]
             break
-update_page_link = "/p/" + pagename_list[0][11:17]
 
-file_content = file_content.replace("date_last_update", update_date)
+file_content = file_content.replace("update_date", update_date)
+file_content = file_content.replace("update_link", update_page_link)
 file_content = file_content.replace("update_page_title", update_page_title)
-file_content = file_content.replace("update_page_link", update_page_link)
 file_content = file_content.replace("update_page_num", str(len(update_list)))
 
 with open(file_path, "w+", encoding = "utf-8-sig") as f :
